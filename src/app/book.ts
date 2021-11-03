6b786861f38fad65db91cfd1e36aff2c32deb1eb
Bashar-prog <S00201189@mail.itsligo.ie>
2021-11-03 17:38:18 +0400

    Angular start

---
+++ src/app/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    _id: any
+    title: string,
+    year_written: number,
+    edition: string,
+    price: number,
+    author : { name : string, nationality : string},
+    tags: string[];
+
+}
+
